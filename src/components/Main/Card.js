1ddb5fb2287e531a929876c37518e12ad2a4d088
ra72e6 <ra72e6@hanmail.net>
2024-06-14 23:24:42 +0900

    0614st

---
+++ src/components/Main/Card.js
@@ -0,0 +1,42 @@
+/* import Library */
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+function Card(props) {
+  let history = useHistory();
+
+  /* Detail 페이지로 이동하기 */
+  const goDetailPage = (id) => {
+    history.push('/detail/' + id);
+  };
+
+  /* 숫자에 콤마(,)추가하여 표시 */
+  const addComma = (num) => {
+    let regexp = /\B(?=(\d{3})+(?!\d))/g;
+    return num.toString().replace(regexp, ',');
+  };
+
+  return (
+    <div
+      className="col-lg-4 col-md-6 opacity"
+      key={props.id}
+      style={{ cursor: 'pointer' }}
+      onClick={() => {
+        goDetailPage(props.shoes.id);
+      }}
+    >
+      <img
+        src={
+          'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'
+        }
+        width="100%"
+      />
+      <h4>{props.shoes.title}</h4>
+      <p>
+        {props.shoes.content} & {addComma(props.shoes.price)}
+      </p>
+    </div>
+  );
+}
+
+export default Card;
